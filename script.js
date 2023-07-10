5079f1c5f6fc9200b18ec9beed7c1bbc7e0e310f
Carolina Serrano <adrianaserrano186@outlook.com>
2023-07-09 21:53:56 -0600

    Add functional range input to change the grid size

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
-for(i=1;i<=256;i++){    
-let gridDiv=document.createElement("div");
-gridDiv.classList.add('gridDiv');
-gridDiv.addEventListener("mouseover",function(){
-    gridDiv.classList.add('gridDivColor')
-})
-document.getElementById('gridContainer').appendChild(gridDiv)
-}
+    changeGrid()
+    let rangeInput = document.getElementById('gridSize');
+    let rangeLabel = document.getElementById('gridSizeLabel');
+    rangeInput.addEventListener('input', function() {
+    rangeLabel.innerText = rangeInput.value+"x"+rangeInput.value;
+    changeGrid()
+    });
 });
 
 function erase(){
     let gridDiv=Array.from(document.getElementsByClassName("gridDiv"))
-    
     gridDiv.forEach(function(square){
         square.classList.remove('gridDivColor')
     });
+}
+
+function changeGrid(){
+    let rangeInput = document.getElementById('gridSize');    
+    let gridSquareSize=400/rangeInput.value;
+    let gridSquareQuantity=Math.round(160000/(gridSquareSize**2));
+    let subcontainer=document.createElement('div');
+    let fragment = document.createDocumentFragment();
+    let gridContainer = document.getElementById('gridContainer');
+    let existingSubcontainer = document.getElementById('subcontainer');
+    subcontainer.setAttribute('id','subcontainer');
+
+    for (let i = 1; i <= gridSquareQuantity; i++) {
+        let gridDiv = document.createElement("div");
+        gridDiv.classList.add('gridDiv');
+        gridDiv.style.height = gridSquareSize + "px";
+        gridDiv.style.width = gridSquareSize + "px";
+        gridDiv.addEventListener("mouseover", function() {
+        gridDiv.classList.add('gridDivColor');
+        });
+        fragment.appendChild(gridDiv);
+    }
+    
+    if (existingSubcontainer) {
+    gridContainer.replaceChild(subcontainer, existingSubcontainer);
+    } else {
+    gridContainer.appendChild(subcontainer);
+    }
+
+    subcontainer.appendChild(fragment);
 }
